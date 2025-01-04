86cdc9c4d8c8516678c6a006be63ff105b6efdc6
Wiibuntu <146908244+Wiibuntu@users.noreply.github.com>
2025-01-03 19:19:42 -0500

    Lots of changes

--- minecraft_clone.c
+++ minecraft_clone.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GL/glew.h>
 #include <GL/glut.h>
 #include <math.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -16,15 +19,40 @@ WorldType selectedWorldType;
 float world[WORLD_SIZE][WORLD_SIZE];
 float cameraX, cameraY, cameraZ;
 float cameraAngleX = 30.0f, cameraAngleY = 45.0f;
+GLuint textures[4];
 
-// Perlin noise for heightmap generation
 float perlinNoise2D(float x, float y) {
     int n = (int)x + (int)y * 57;
     n = (n << 13) ^ n;
     return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
 }
 
-// Generate terrain based on selected world type
+void loadTextures() {
+    int width, height, channels;
+    unsigned char* image;
+
+    glGenTextures(4, textures);
+
+    const char* texturePaths[4] = {
+        "textures/grass.png",
+        "textures/sand.png",
+        "textures/forest.png",
+        "textures/rock.png"
+    };
+
+    for (int i = 0; i < 4; i++) {
+        glBindTexture(GL_TEXTURE_2D, textures[i]);
+        image = stbi_load(texturePaths[i], &width, &height, &channels, STBI_rgb_alpha);
+        if (image) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+            gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, width, height, GL_RGBA, GL_UNSIGNED_BYTE, image);
+        } else {
+            exit(1);
+        }
+        stbi_image_free(image);
+    }
+}
+
 void generateTerrain() {
     float frequency;
     switch (selectedWorldType) {
@@ -51,33 +79,36 @@ void generateTerrain() {
     }
 }
 
-// Set camera above the terrain at a random point
 void setCameraAboveTerrain() {
     int randX = rand() % WORLD_SIZE;
     int randZ = rand() % WORLD_SIZE;
     cameraX = randX;
     cameraZ = randZ;
-    cameraY = world[randX][randZ] + 30.0f;  // Camera height above terrain
+    cameraY = world[randX][randZ] + 30.0f;
 }
 
-// Initialize OpenGL settings
 void initGL() {
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        exit(1);
+    }
     glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0, (double)WIDTH / (double)HEIGHT, 0.1, 500.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
-// Generate the world and position the camera
 void generateWorld() {
     selectedWorldType = (WorldType)(rand() % 4);
     generateTerrain();
     setCameraAboveTerrain();
+    loadTextures();
 }
 
-// Handle keyboard input for camera movement
 void handleKeys(unsigned char key, int x, int y) {
     switch (key) {
         case 'w':
@@ -104,13 +135,14 @@ void handleKeys(unsigned char key, int x, int y) {
     glutPostRedisplay();
 }
 
-// Render the terrain
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     gluLookAt(cameraX, cameraY, cameraZ,
               cameraX, cameraY - 10.0f, cameraZ - 10.0f,
               0.0f, 1.0f, 0.0f);
+
+    glBindTexture(GL_TEXTURE_2D, textures[selectedWorldType]);
 
     for (int x = 0; x < WORLD_SIZE - 1; x++) {
         for (int z = 0; z < WORLD_SIZE - 1; z++) {
@@ -119,18 +151,16 @@ void display() {
             float nextHeightZ = world[x][z + 1];
 
             glBegin(GL_QUADS);
-                glColor3f(0.3f + height / HEIGHT_SCALE, 0.8f, 0.3f);
-                glVertex3f(x, height, z);
-                glVertex3f(x + 1, nextHeightX, z);
-                glVertex3f(x + 1, nextHeightX, z + 1);
-                glVertex3f(x, nextHeightZ, z + 1);
+                glTexCoord2f(0.0f, 0.0f); glVertex3f(x, height, z);
+                glTexCoord2f(1.0f, 0.0f); glVertex3f(x + 1, nextHeightX, z);
+                glTexCoord2f(1.0f, 1.0f); glVertex3f(x + 1, nextHeightX, z + 1);
+                glTexCoord2f(0.0f, 1.0f); glVertex3f(x, nextHeightZ, z + 1);
             glEnd();
         }
     }
     glutSwapBuffers();
 }
 
-// Idle loop to keep refreshing display
 void idle() {
     glutPostRedisplay();
 }
